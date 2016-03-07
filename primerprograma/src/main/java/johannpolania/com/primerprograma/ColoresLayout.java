1830a16ff4b57de2586bb394346bc9a607fb9955
koanco <koanco@hotmail.com>
2016-03-07 17:27:00 -0500

    punto1

---
+++ primerprograma/src/main/java/johannpolania/com/primerprograma/ColoresLayout.java
@@ -0,0 +1,13 @@
+package johannpolania.com.primerprograma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColoresLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colores_layout);
+    }
+}
